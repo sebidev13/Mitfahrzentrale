e4452cbf9fb6a91ad4d0f3b259aa805610721b12
sebidev13 <135363303+sebidev13@users.noreply.github.com>
2024-01-23 09:53:31 +0100

    Login UI

--- App/app/src/main/java/com/example/myapplication/activities/LoginActivity.kt
+++ App/app/src/main/java/com/example/myapplication/activities/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.Constants
 import com.example.myapplication.R
@@ -13,6 +14,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //TODO: Texte an Service übergeben
+        val user_input = findViewById<EditText>(R.id.userInput)
+        val user_txt: String = user_input.text.toString()
+
+        val pwd_input = findViewById<EditText>(R.id.passwordInput)
+        val pwd_txt: String = pwd_input.text.toString()
+
+        //TODO: Prüfung für Logindaten hinzufügen und ggf. user an intent übergeben
         val login_btn = findViewById<Button>(R.id.login)
         login_btn.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
